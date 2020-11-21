f4d9057b39892a972cbee818564e9b82b1ab70ef
srajvanshi3 <shivam99rajvanshi@gmail.com>
2020-11-21 12:44:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Cart from "./components/Cart/Cart"
+import Default from "./components/Default"
+import Details from "./components/Details"
+import Navbar from "./components/Navbar"
+import ProductList from "./components/ProductList"
+import Modal from './components/Modal'
+import{BrowserRouter , Switch , Route} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+      <Switch>
+      <Route exact path="/" component={ProductList}/>
+      <Route exact path="/details" component={Details}/>
+      <Route exact path="/cart" component={Cart}/>
+      <Route  component={Default} />
+
+      </Switch>
+      <Modal/>
+      
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import{BrowserRouter , Switch , Route} from "react-router-dom"
+import {ProductProvider} from './context'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ProductProvider>
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>
+  
+  </ProductProvider>
+    
+  ,
   document.getElementById('root')
 );
 
